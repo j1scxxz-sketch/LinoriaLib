69d67994082c32dfe33fd16fc5f4be92cabe10d1
j1scxxz-sketch <j1scxxz@gmail.com>
2026-02-04 17:18:46 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1808,7 +1808,7 @@ do
             SubButton.Outer, SubButton.Inner, SubButton.Label = CreateBaseButton(SubButton)
 
 SubButton.Outer.Position = UDim2.new(1, 3, 0, 0)
-            SubButton.Outer.Size = UDim2.new(0.5, -2.5, 1, 0)
+            SubButton.Outer.Size = UDim2.fromOffset(self.Outer.AbsoluteSize.X - 2, self.Outer.AbsoluteSize.Y)
             SubButton.Outer.Parent = self.Outer
 
             function SubButton:AddTooltip(tooltip)
